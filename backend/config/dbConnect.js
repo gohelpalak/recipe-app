b10ed2d1b7203e7329ea07658b83f36500c56f80
palak <palakgohel14@gmail.com>
2025-03-28 15:02:06 +0530

    admin

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,8 +1,7 @@
 const mongoose= require("mongoose")
 
 const dbConnect=()=>{
-    mongoose.connect("mongodb+srv://gohelpalak14:palak%4022@cluster0.cjwxl.mongodb.net/Recipe")
-    // mongoose.connect("mongodb+srv://PraveshPrajapati:Pravesh%402004@cluster0.v4bed.mongodb.net/Recipe")
+    mongoose.connect("mongodb+srv://gohelpalak14:palak%4022@cluster0.cjwxl.mongodb.net/Recipe-MERN")
     .then((res) => {
         console.log("Db connect");
     }).catch((err) => {
